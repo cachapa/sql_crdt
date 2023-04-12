9dcd865d88bbe24b170bb9b1f88fa5cd08313f1f
Daniel Cachapa <cachapa@gmail.com>
2023-04-12 22:47:43 +0100

    Add example

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.9+2
+
+- Add example file
+
 ## 0.0.9+1
 
 - Add documentation
---
+++ example/example.dart
@@ -0,0 +1,7 @@
+Future<void> main() async {
+  print(
+      'No example here since this is package is a generic implementation that '
+      'depends on a backing SQL database to achieve its functionality.\n\n'
+      'Check https://github.com/cachapa/sqlite_crdt or '
+      'https://github.com/cachapa/postgres_crdt for actual implementations.');
+}
